587ca8a037e27bc38bfe89a50ec10750db325273
Gant06 <hectormigueldefinitivo@gmail.com>
2026-02-15 20:26:42 +0100

    Componente BoatInfoCard

--- bombava-frontend/src/componentes/BoatInfoCard.jsx
+++ bombava-frontend/src/componentes/BoatInfoCard.jsx
@@ -43,4 +43,54 @@ function HealthBar({ maxHealth = 100, currentHealth = 0 }) {
     );
 } 
 
-export { HealthBar };
+
+function ModuleInfo({ moduleName, moduleImg, moduleAlt, moduleMaxHealth, moduleHealth }) {
+    return (
+        <div className='module-info'>
+            <h4>{moduleName}</h4>
+            <div className='module-display'>
+                <img src={moduleImg} alt={moduleAlt} className='module-image' />
+                <HealthBar maxHealth={moduleMaxHealth} currentHealth={moduleHealth} />
+            </div>
+        </div>
+    )
+}
+
+function BoatInfoCard({ boatId }) {
+
+    // En caso de que no haya un barco seleccionado
+    if (!boatId) {
+        return( 
+            <div className='boat-info-card'>
+                <p>Selecciona un barco para ver su estado</p>
+            </div>
+        )
+    }
+
+    const [boatdata, setBoatData] = useState();
+
+    //Por Hacer: Cargar la informacion del barco a traves de la API.
+
+    // Caso de barco seleccionado.
+    return (
+        <div className='boat-info-card'>
+            <h2>{boatdata.boatName}</h2>
+            <img src={boatdata.boatImg} alt='' className='boat-image' />
+            <ol>
+                {boatdata.modules.map((module => (
+                    <li key={module.moduleId}>
+                        <ModuleInfo 
+                            moduleName={module.moduleName}
+                            moduleImg={module.moduleImg}
+                            moduleAlt={module.moduleAlt}
+                            moduleMaxHealth={module.moduleMaxHealth}
+                            moduleHealth={module.moduleHealth}
+                        />
+                    </li>
+                )))}
+            </ol>
+        </div>
+    )
+}
+
+export default BoatInfoCard;
