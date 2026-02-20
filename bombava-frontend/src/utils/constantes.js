ee716d7b4cb21c5fb7adae929d8613667c2a5d7e
Luis-b-c <897140@unizar.es>
2026-02-20 01:54:48 +0000

    Fix: Mejoras visuales del Tablero y Barcos

--- bombava-frontend/src/utils/constantes.js
+++ bombava-frontend/src/utils/constantes.js
@@ -6,12 +6,15 @@ export const TAMANO_TABLERO = 15;
 export const TAMANO_CELDA = 40;
 
 export const TERRENO = {
-  AGUA: 'agua',     // Agua profunda 
+  AGUA: 'agua',     // Agua profunda
   ISLA: 'isla',   // Isla 
+  AGUA_NO_PROFUNDA: 'agua_no_profunda' 
 };
 
 // Configuración visual temporal, mientras no haya imagenes para islas, ...
 export const COLORES_TERRENO = {
   [TERRENO.AGUA]: '#1e88e5',    
-  [TERRENO.ISLA]: '#aed581',  
+  [TERRENO.ISLA]: '#aed581',
+  [TERRENO.AGUA_NO_PROFUNDA]: '#a6cff3'
+, 
 };
