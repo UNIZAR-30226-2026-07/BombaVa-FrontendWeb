38c4761a7cc3a2a08b7b7c6028040315474a9fcd
Gant06 <hectormigueldefinitivo@gmail.com>
2026-02-23 13:21:40 +0100

    Arreglado el uso de rutas con UseNavigate

--- bombava-frontend/src/App.jsx
+++ bombava-frontend/src/App.jsx
@@ -1,42 +1,23 @@
 // App.jsx
+import { Routes, Route, useLocation, Navigate } from "react-router-dom";
 import { useState } from 'react';
-import Menu from './componentes/MenuInicio';
-import Perfil from './componentes/Perfil';
+import Menu from './pantallas/MenuInicio';
+import Perfil from './pantallas/Perfil';
 import InfoCard from './componentes/BoatInfoCard'
 
 
 function App() {
   const [pantalla, setPantalla] = useState('inicio');
 
-  const [usuario, setUsuario] = useState({
-    nombre: "Capitán Pruebas",
-    rango: "Grumete de Agua Dulce",
-    partidas: 42,
-    victorias: 15,
-    precision: "68%"
-  });
-
-  // Variable para guardar lo que se genera
-  let contenido;
-  // Aquí guardamos los datos del usuario (el "Estado")
-
-  // Depende de la pantalla en la que estés cambias el estado a otra
-  if (pantalla === 'inicio') {
-    contenido = <Menu alClickJugar={(destino) => setPantalla(destino)} />;//Le paso al componente hijo (MenuInicio) una funcion que espera recibir un argumento destino
-  }
-  else if (pantalla == 'perfil'){
-    // Tengo que pasar 2 cosas: los datos del perfil y para volver
-    contenido = (
-      <Perfil datos={usuario} 
-        alVolver={() => setPantalla('inicio')} 
-      />
-    );
-  }
-
   return (
-    <div className="main-container">
-      <InfoCard boatId={1} id='app-infoCard' />
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Menu alClickJugar={(destino) => setPantalla(destino)} />} />
+        <Route path="/info" element={<InfoCard boatId={1} />} />
+        <Route path="/profile" element={<Perfil datos={usuario} />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </>
   );
 }
 
--- bombava-frontend/src/pantallas/Perfil.jsx
+++ bombava-frontend/src/pantallas/Perfil.jsx
@@ -1,9 +1,22 @@
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import '../styles/Perfil.css';
 
-function Perfil({ datos, alVolver }) {
+function Perfil() {
+
+  const navigate = useNavigate();
+
+  const [usuario, setUsuario] = useState({
+    nombre: "Capitán Pruebas",
+    rango: "Grumete de Agua Dulce",
+    partidas: 42,
+    victorias: 15,
+    precision: "68%"
+  });
+
   return ( 
     <div className="contenedor_perfil">
-      <button className="boton_cerrar" onClick={alVolver}>{/*Faltaría poner la función para quitar la cuenta*/}
+      <button className="boton_cerrar" onClick={() => navigate('/')}>{/*Faltaría poner la función para quitar la cuenta*/}
         CERRAR SESIÓN
       </button>
       <header className="titulo_perfil"> 
@@ -14,17 +27,17 @@ function Perfil({ datos, alVolver }) {
       </div>
     {/* AQUÍ SACAMOS LOS DATOS */}
       <div className="info_capitan">
-        <h2>{datos.nombre}</h2>
-        <p className="rango">{datos.rango}</p>
+        <h2>{usuario.nombre}</h2>
+        <p className="rango">{usuario.rango}</p>
         
         <div className="estats">
-          <p><strong>Partidas:</strong> {datos.partidas}</p>
-          <p><strong>Victorias:</strong> {datos.victorias}</p>
-          <p><strong>Precisión:</strong> {datos.precision}</p>
+          <p><strong>Partidas:</strong> {usuario.partidas}</p>
+          <p><strong>Victorias:</strong> {usuario.victorias}</p>
+          <p><strong>Precisión:</strong> {usuario.precision}</p>
         </div>
       </div>
 
-      <button className="boton_volver" onClick={alVolver}>
+      <button className="boton_volver" onClick={() => navigate('/')}>
         VOLVER AL PUERTO
       </button>
     </div>
