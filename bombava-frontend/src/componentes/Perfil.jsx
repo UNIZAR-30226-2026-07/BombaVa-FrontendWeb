5f7ecf5608efde560b650c4c444d73d90b7733ba
Gant06 <hectormigueldefinitivo@gmail.com>
2026-02-23 18:31:53 +0100

    FIX: Reorganizados estilos Perfil

--- bombava-frontend/src/componentes/Perfil.jsx
+++ bombava-frontend/src/componentes/Perfil.jsx
@@ -6,21 +6,23 @@ function Perfil({ datos, alVolver }) {
       <button className="boton_cerrar" onClick={alVolver}>{/*Faltaría poner la función para quitar la cuenta*/}
         CERRAR SESIÓN
       </button>
-      <header className="titulo_perfil"> 
-        <h1>PERFIL DEL CAPITÁN</h1>
-      </header>
-      <div className="circulo_perfil">
-        ☠️
-      </div>
-    {/* AQUÍ SACAMOS LOS DATOS */}
-      <div className="info_capitan">
-        <h2>{datos.nombre}</h2>
-        <p className="rango">{datos.rango}</p>
-        
-        <div className="estats">
-          <p><strong>Partidas:</strong> {datos.partidas}</p>
-          <p><strong>Victorias:</strong> {datos.victorias}</p>
-          <p><strong>Precisión:</strong> {datos.precision}</p>
+      <div className="main-content">
+        <header className="titulo_perfil"> 
+          <h1>PERFIL DEL CAPITÁN</h1>
+        </header>
+        <div className="circulo_perfil">
+          ☠️
+        </div>
+      {/* AQUÍ SACAMOS LOS DATOS */}
+        <div className="info_capitan">
+          <h2>{datos.nombre}</h2>
+          <p className="rango">{datos.rango}</p>
+          
+          <div className="estats">
+            <p><strong>Partidas:</strong> {datos.partidas}</p>
+            <p><strong>Victorias:</strong> {datos.victorias}</p>
+            <p><strong>Precisión:</strong> {datos.precision}</p>
+          </div>
         </div>
       </div>
 
