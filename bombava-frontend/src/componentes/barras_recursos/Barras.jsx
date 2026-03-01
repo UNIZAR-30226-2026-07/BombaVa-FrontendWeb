012581bffd1abc7838b7f82f1135b3eef276e312
Luis-b-c <897140@unizar.es>
2026-03-01 23:36:19 +0000

    FIX:  Mejora de la legibilidad del código

--- bombava-frontend/src/componentes/barras_recursos/Barras.jsx
+++ bombava-frontend/src/componentes/barras_recursos/Barras.jsx
@@ -7,6 +7,7 @@ import iconoGasolina from "../../assets/barras_recursos/gasolina.png";
 const BarraProgreso = ({ etiqueta, maxValor = 100, valorActual = 0, tipo }) => {
     const porcentaje = (valorActual / maxValor) * 100;
 
+    // Determinar el icono según el tipo de barra (munición o combustible)
     let imagenSrc;
     if(tipo === "municion") {
         imagenSrc = iconoBalas;
@@ -17,17 +18,20 @@ const BarraProgreso = ({ etiqueta, maxValor = 100, valorActual = 0, tipo }) => {
 
     return (
         <div className="barra-wrapper">
+            {/*Icono a la izquierda de la barra, dependiendo del tipo*/}
             <img src={imagenSrc} alt={tipo} className="barra-icono" />
+            {/*Contenedor de la barra con el nombre y el relleno*/}
             <div className={`barra-container ${tipo}`}>
                 {/*Nombre que se ve*/}
                 <span className="barra-nombre">{etiqueta}</span>
-                
+
+                {/*Relleno de la barra*/}
                 <div 
                     className="barra-contenido" 
                     style={{ width: `${porcentaje}%` }} 
                 />
             </div>
-            {/*Barra con los numeros*/}
+            {/*Números de como de avanzada esta  la barra*/}
             <span className="barra-resumen">
                 {valorActual} / {maxValor}
             </span>
