bcf290d51aec45ebb38f5ce96e7798ddf50d5cea
Luis <233235385+Luis-b-c@users.noreply.github.com>
2026-04-22 12:39:03 +0200

    IMP: Mejora visual menu. Información sobre los modos de juego con pergaminos

--- bombava-frontend/src/pantallas/MenuInicio.jsx
+++ bombava-frontend/src/pantallas/MenuInicio.jsx
@@ -27,24 +27,36 @@ function MenuInicio({ }) {
         <div className="opcion-mapa config" onClick={() => navigate('/configurar')}>
           <div className="tablon-opcion">CONFIGURAR FLOTA</div>
           <img src={iconoMartillo} alt="Martillo" className="icono-opcion" />
+          <div className="pergamino">
+            Prepara tus barcos, elige tu armamento y diseña una formación para ganar la batalla.
+          </div>
         </div>
 
         {/* Competitivo */}
         <div className="opcion-mapa competitivo" onClick={() => navigate('/combate')}>
           <div className="tablon-opcion">COMPETITIVO</div>
           <img src={iconoCalavera} alt="Calavera y dos espadas" className="icono-opcion" />
+          <div className="pergamino">
+            Enfréntate a otros capitanes en batallas navales.
+          </div>
         </div>
 
         {/* Práctica */}
         <div className="opcion-mapa practica" onClick={() => navigate('/sala-espera')}>
           <div className="tablon-opcion">PRÁCTICA</div>
           <img src={iconoBarril} alt="Barril" className="icono-opcion" />
+          <div className="pergamino">
+            Perfecciona tu puntería sin riesgo.
+          </div>
         </div>
 
         {/* Unirse */}
         <div className="opcion-mapa unirse" onClick={() => navigate('/unirse')}>
           <div className="tablon-opcion">UNIRSE</div>
           <img src={iconoBrujula} alt="Brujula" className="icono-opcion" />
+          <div className="pergamino">
+            Entra en una batalla ya creada introduciendo el código de sala de tu rival.
+          </div>
         </div>
 
       </div>
